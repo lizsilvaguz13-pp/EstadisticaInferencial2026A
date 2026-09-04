31d67810beffa68fe2abcfc707e494f80f16cc26
lizsilvaguz13-pp <lizsilvaguz13@gmail.com>
2026-09-04 09:40:01 -0600

    Create cargar_datos.py

---
+++ Unidad1/codigos/cargar_datos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+link_csv = "https://raw.githubusercontent.com/lizsilvaguz13-pp/EstadisticaInferencial2026A/refs/heads/main/Unidad1/problemario/dato_problema_01.csv"
+
+df = pd.read_csv(link_csv)
+
+df
